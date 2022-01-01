d4e04541605f6a8d66355d2568d5594a902e52e9
Andromeda <andrei.froschgaizer@yandex.ru>
2022-01-02 00:47:22 +0700

    created and setup the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { Layout, Alert, Col } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Header>Header</Header>
+
+        <Col span="8" offset="8">
+        <Alert message="Authentication Failed" type="warning"></Alert>
+        </Col>
+          <header className="App-header">
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://ru.reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+
+            <a
+              className="App-link"
+              href="https://ru.reactjs1.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+    </div >
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss'
+
+class Header extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <header className='header'>
+                Header
+            </header>
+        )
+    }
+}
+
+export default Header
